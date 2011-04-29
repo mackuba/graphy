7f07fdeb358ffd37b04395ec5da2480d641ad48e
Jakub Suder <jakub.suder@gmail.com>
2011-04-29 17:38:45 +0200

    allow different value units for different monitors

--- lib/graphy/monitors.rb
+++ lib/graphy/monitors.rb
@@ -1,27 +1,43 @@
 module Graphy
+  class Monitor
+    attr_accessor :name, :unit
+
+    def initialize(name, unit, block)
+      @name = name
+      @unit = unit
+      @block = block
+    end
+
+    def call(process, set)
+      @block.call(process, set)
+    end
+  end
+
   class << self
     def monitors
       @monitors ||= {}
     end
 
-    def add_monitor(name, proc = nil, &block)
-      monitors[name] = proc || block
+    def add_monitor(name, unit, proc = nil, &block)
+      monitors[name] = Monitor.new(name, unit, proc || block)
     end
 
-    def process_filter_monitor(ps_field)
+    def process_filter_monitor(ps_field, divisor = nil)
       Proc.new do |process, set|
         ps = `ps ax -o #{ps_field},command`
         sum = 0
         ps.each_line do |line|
           if line.include?(process.name)
-            sum += line.strip.split(/\s+/).first.to_i
+            value = line.strip.split(/\s+/).first.to_i
+            value = value.to_f / divisor if divisor
+            sum += value
           end
         end
-        sum
+        divisor ? sprintf("%.1f", sum) : sum
       end
     end
   end
 
-  add_monitor :memory, process_filter_monitor('rss')
-  add_monitor :cpu, process_filter_monitor('pcpu')
+  add_monitor :memory, 'M', process_filter_monitor('rss', 1000)
+  add_monitor :cpu, '%', process_filter_monitor('pcpu')
 end
