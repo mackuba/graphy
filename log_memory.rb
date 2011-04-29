ad40ba87589eda63fd1fa57d7841c2ce8f3e2696
Jakub Suder <jakub.suder@gmail.com>
2011-04-29 11:06:55 +0200

    initial commit

---
+++ log_memory.rb
@@ -0,0 +1,170 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+ROOT_DIR = "/var/lib/memory"
+FILES = ["memory.conf", "index.html", "memory.js", "dygraph-combined.js"]
+
+module Memory
+  class Process
+    attr_accessor :name, :options
+
+    def initialize(name, options = {})
+      @name = name.to_s
+      @options = options
+    end
+  end
+
+  class Config
+    def initialize(parent)
+      @parent = parent
+    end
+
+    def schedule(string)
+      @parent.schedule = string
+    end
+
+    def process(name, options = {})
+      @parent.processes << Process.new(name, options)
+    end
+  end
+
+  class << self
+    attr_accessor :schedule, :processes
+
+    def processes
+      @processes ||= []
+    end
+
+    def configure
+      yield Config.new(self)
+    end
+  end
+end
+
+case ARGV.first
+  when "install"
+    begin
+      Dir.mkdir(ROOT_DIR) unless File.directory?(ROOT_DIR)
+    rescue SystemCallError
+      puts "#{ROOT_DIR} can't be created - run this command with 'sudo' or 'rvmsudo'"
+      exit 1
+    end
+
+    # ask to overwrite
+    FILES.each { |f| FileUtils.cp(f, ROOT_DIR) }
+
+    if Process.euid == 0
+      real_user = ENV['SUDO_USER']
+      if real_user
+        uid = `id -u #{real_user}`.to_i
+        gid = `id -g #{real_user}`.to_i
+        files = FILES.map { |f| File.join(ROOT_DIR, f) }
+        File.chown(uid, gid, ROOT_DIR, *files)
+      end
+    end
+
+  when "log"
+    unless File.exist?(File.join(ROOT_DIR, "memory.conf"))
+      puts "No memory.conf file - please run install."
+      exit 1
+    end
+
+    load File.join(ROOT_DIR, "memory.conf")
+
+    data = []
+
+    data << Time.now.to_i
+
+    # memory_stats = `free -k -o | grep Mem`
+    # data << memory_stats.strip.split(/\s+/)[2].to_i
+
+    Memory.processes.each do |process|
+      ps = `ps ax -o rss,command`
+      sum = 0
+      ps.each_line do |line|
+        if line.include?(process.name)
+          sum += line.strip.split(/\s+/)[0].to_i
+        end
+      end
+      data << sum
+    end
+
+    csv = File.join(ROOT_DIR, "log.csv")
+    existed = File.exist?(csv)
+
+    File.open(csv, "a") do |f|
+      f.write("time," + Memory.processes.map(&:name).join(",") + "\n") unless existed
+      f.write(data.join(",") + "\n")
+    end
+
+  when "update"
+    # update csv
+    # update index / js
+
+    unless File.exist?(File.join(ROOT_DIR, "memory.conf"))
+      puts "No memory.conf file - please run install."
+      exit 1
+    end
+
+    load File.join(ROOT_DIR, "memory.conf")
+
+    unless Memory.schedule
+      puts "No schedule - please update your memory.conf."
+      exit 1
+    end
+
+    crontab = `crontab -l 2> /dev/null`.split(/\n/)
+    old_line = crontab.grep(/# memory gem/).first
+    rvm_path = ENV['rvm_path']
+    rvm_load = "source #{rvm_path}/scripts/rvm &&" if rvm_path
+    new_line = "#{Memory.schedule}     #{rvm_load} cd #{ROOT_DIR} && ruby log_memory.rb   # memory gem"
+
+    if old_line
+      old_line.replace(new_line)
+    else
+      crontab << new_line
+    end
+
+    crontab_file = File.join(ROOT_DIR, "crontab")
+    File.open(crontab_file, "w") { |f| f.write(crontab.map { |l| l + "\n" }.join) }
+    system("crontab #{crontab_file}")
+    File.unlink(crontab_file)
+
+    # update logs
+    Dir[ROOT_DIR + "/log.csv*"].each { |f| File.unlink(f) }
+
+  when "remove"
+    crontab = `crontab -l 2> /dev/null`.split(/\n/)
+    old_line = crontab.grep(/# memory gem/).first
+
+    if old_line
+      crontab.delete(old_line)
+      crontab_file = File.join(ROOT_DIR, "crontab")
+      File.open(crontab_file, "w") { |f| f.write(crontab.map { |l| l + "\n" }.join) }
+      system("crontab #{crontab_file}")
+      File.unlink(crontab_file)
+    end
+
+  when "server"
+    require 'webrick'
+    server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => ROOT_DIR
+    server.start
+
+  when "purge"
+    begin
+      if File.directory?(ROOT_DIR)
+        print "Deleting all data - are you sure? (y/n) "
+        if STDIN.gets.strip == "y"
+          Dir[ROOT_DIR + "/*"].each { |f| File.unlink(f) }
+          Dir.rmdir(ROOT_DIR)
+        end
+      end
+    rescue SystemCallError => e
+      puts "#{ROOT_DIR} can't be deleted - run this command with 'sudo' or 'rvmsudo'"
+      exit 1
+    end
+
+  else
+    puts "#{$0} log|server|install"
+end
---
+++ memory.js
@@ -0,0 +1,28 @@
+var Memory = {
+
+  GRAPH_WIDTH: 1000,
+
+  drawGraph: function(file) {
+    var div = document.createElement('div');
+    div.style.width = Memory.GRAPH_WIDTH + 'px';
+    document.getElementsByTagName('body')[0].appendChild(div);
+
+    new Dygraph(div, file, {
+      labels: [
+        "time", "websocket_server", "flash_policy", "resque", "bluepilld",
+        "redis-server", "mongodb", "Rack", "Passenger", "nginx"
+      ],
+      labelsDivWidth: 150,
+      labelsDivStyles: {
+        'margin-left': '175px',
+        'line-height': '150%'
+      },
+      showRoller: true,
+      xAxisLabelFormatter: Dygraph.dateAxisFormatter,
+      xTicker: Dygraph.dateTicker,
+      xValueFormatter: Dygraph.dateString_,
+      xValueParser: function(x) { return parseInt(x, 10) * 1000 },
+      yValueFormatter: function(x) { return " " + Dygraph.intFormat(Math.floor(x / 100) / 10) + "M" }
+    });
+  }
+};
