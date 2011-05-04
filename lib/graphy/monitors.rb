d24ac0ed4adc33fcbcdaabcf00985aa855f8be35
Jakub Suder <jakub.suder@gmail.com>
2011-05-04 12:12:39 +0200

    added some comments for add_monitor

--- lib/graphy/monitors.rb
+++ lib/graphy/monitors.rb
@@ -18,6 +18,22 @@ module Graphy
       @monitors ||= {}
     end
 
+    #
+    # usage:
+    #
+    # Graphy.add_monitor(:disk_space, 'MB') do |watch, set|
+    #   result = `du -sm #{watch.name}`
+    #   result.split(/\s+/).first.to_i
+    # end
+    #
+    # and then, in the config file:
+    #
+    # g.monitor :disk_space do |m|
+    #   m.watch "/var/lib/mongodb", :label => "MongoDB"
+    #   m.watch "/var/www/foo/shared/public", :label => "Uploaded files"
+    # end
+    #
+
     def add_monitor(name, unit, proc = nil, &block)
       monitors[name] = Monitor.new(name, unit, proc || block)
     end
